3952e8c8fb72689dda1fe78aa26c8c2410ff7112
mewu <mewu@idmod.org>
2024-08-28 12:06:17 -0700

    fix port issue

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -13,13 +13,10 @@ if [ -f "R/renv.lock" ]; then
     cd R && R -e 'renv::restore()'
 fi
 
-# Go back to the root directory
-cd /workspace
-
 # Set up Jupyter notebooks for Python and R
 echo "Setting up Jupyter notebooks..."
-mkdir -p ~/.jupyter && echo \"c.NotebookApp.token = ''\" >> ~/.jupyter/jupyter_notebook_config.py
+mkdir -p /home/vscode/.jupyter && echo \"c.NotebookApp.token = ''\" >> /home/vscode/.jupyter/jupyter_notebook_config.py
 
-jupyter notebook --config=~/.jupyter/jupyter_notebook_config.py
+jupyter notebook --config=/home/vscode/jupyter_notebook_config.py
 
 echo "Dev environment setup completed!"
