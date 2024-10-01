4180e5351f3ec4af760b04600ef816b3e8d3fd76
mewu <mewu@idmod.org>
2024-09-30 18:34:34 -0700

    simplify the postXrate script

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -19,26 +19,4 @@ fi
 #    cd r && R -e 'renv::restore()'
 #fi
 
-# Delay to allow the environment to fully initialize
-sleep 10
-
-# change the default working directory for RStudio
-echo "session-default-working-dir=${CODESPACE_VSCODE_FOLDER}" | sudo tee -a /etc/rstudio/rsession.conf
-
-# Start RStudio Server
-rstudio-server start > rstudio_server.log 2>&1 &
-sleep 5  # Give some time for the service to start
-if ! pgrep -x "rstudio-server" > /dev/null; then
-    echo "RStudio Server failed to start. Restarting..."
-    rstudio-server restart > rstudio_server.log 2>&1 &
-fi
-
-# Start Jupyter Lab
-jupyter lab --ip=0.0.0.0 --no-browser --allow-root --NotebookApp.token='' > jupyter_lab.log 2>&1 &
-sleep 5
-if ! pgrep -x "jupyter-lab" > /dev/null; then
-    echo "Jupyter Lab failed to start. Restarting..."
-    jupyter lab --ip=0.0.0.0 --no-browser --allow-root --NotebookApp.token='' > jupyter_lab.log 2>&1 &
-fi
-
 echo "Dev environment setup completed!"
