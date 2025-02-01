724d7e73e6def825c474ca284d2fd68979086cf9
MeWu-IDM <40474438+MeWu-IDM@users.noreply.github.com>
2025-01-31 19:32:53 -0800

    Add demo website (#11)
    - add slides and quiz

--- generate_links.py
+++ generate_links.py
@@ -21,50 +21,50 @@ def get_title(qmd_file):
     with open(qmd_file, "r", encoding="utf-8") as file:
         for line in file:
             if line.lower().startswith("title:"):
-                return line.split(":", 1)[1].strip()
+                return line.split(":", 1)[1].strip().replace("\"", "")
     return None
 
 
-def generate_quarto_config(root_dir, target_folders):
+def generate_quarto_config(root_dir, target_folders, output_dir="_site"):
     """Generate a Quarto configuration file with links to rendered HTML files."""
     sidebar_contents = []
-    files_to_copy = []
     root_path = Path(root_dir)
     print(root_path)
     for subfolder in root_path.iterdir():
-        print(f"subfolder: {subfolder}")
+        # print(f"subfolder: {subfolder}")
         if subfolder.is_dir() and subfolder.name in target_folders:
             print(f"Processing folder: {subfolder}")
-            for file in subfolder.glob("**/*.html"):
-                if file.stem == "index":
-                    continue
-                print(f"Processing file: {file}")
-                html_path = file
-                relative_link = html_path.relative_to(root_dir)
+            target_files = list(subfolder.glob("**/*.qmd") ) + list (subfolder.glob("**/*.ipynb"))
+            for f in target_files:
                 # Check for corresponding .qmd file to extract the title
-                qmd_file = html_path.with_suffix(".qmd")
-                title = None
-                if qmd_file.exists():
-                    title = get_title(qmd_file)
-                title = relative_link.stem if title is None else title  # Fallback to folder name
-                sidebar_contents.append({"text": title, "href": str(relative_link)})
-                files_to_copy.append(str(relative_link))
+                title = get_title(f)
+                html_file = f.with_suffix(".html")
+                sidebar_contents.append({"text": html_file.stem if title is None else title, "href": str(html_file.relative_to(root_dir).as_posix())})
 
     # Define the Quarto configuration structure
     quarto_config = {
-        "project": {"type": "website", "render" : ["README.qmd"], "output-dir": "gallery",},
+        "project": {"type": "website",
+                    "render": ["**/*.qmd", "**/*.ipynb", "!r/", "!python/", "!gallery/"],
+                    "output-dir": output_dir,
+                    },
         "website": {
             "title": "Knowledge Sharing with Quarto Tutorial Website",
             "description": "A collection of examples using quarto showcasing Python / R code for data access and analysis",
             "navbar": {
-                "left": [{"text": "Home", "href": "README.html"}],
+                "left": [{"text": "Home", "href": "index.html"}],
                 "right": [{"text": "GitHub", "href": "https://github.com/InstituteforDiseaseModeling/data_access_library"}],
             },
             "sidebar": {
-                "contents": [{"section": "Examples Gallery", "contents": sidebar_contents}]
+                "contents": [{"section": "<span class='sidebar-header'>Repo Examples Gallery</span>", "contents": sidebar_contents},
+                             {"section": "<span class='sidebar-header'>Other Quarto Examples</span>", "contents":
+                                 [{"text": "Quarto example by Amelia", "href":"https://bertozzivill.github.io/Principles-and-Practice-of-Data-Visualization-in-R/"}]}]
             }
         },
-        "files": files_to_copy
+        "format": {
+            "html":{
+                "css": "style.css"
+            }
+        }
     }
     return quarto_config
 
@@ -77,7 +77,7 @@ def write_quarto_config(config, output_file="_quarto.yml"):
 
 if __name__ == "__main__":
     root_dir = "."  # Root directory of the repository
-    convert_md_to_qmd( Path(root_dir)/ "README.md", Path(root_dir)/ "README.qmd")
+    # convert_md_to_qmd( Path(root_dir)/ "README.md", Path(root_dir)/ "index.qmd")
     quarto_config = generate_quarto_config(root_dir, ["r", "python"])
     write_quarto_config(quarto_config)
     print("Generated quarto.yml with sidebar links to HTML files.")
